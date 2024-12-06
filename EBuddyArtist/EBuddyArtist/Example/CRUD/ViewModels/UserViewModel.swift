41ebda6729af5b6a8b0bb2827895abadf611ca83
macAji <ajienergystar@gmail.com>
2024-12-06 14:10:06 +0700

    fetch the USERS document and display

---
+++ EBuddyArtist/EBuddyArtist/Example/CRUD/ViewModels/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  EBuddyArtist
+//
+//  Created by mac on 6/12/24.
+//
+
+import Foundation
---
+++ EBuddyArtist/EBuddyArtist/Example/CRUD/Views/UserDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  UserDetailView.swift
+//  EBuddyArtist
+//
+//  Created by mac on 6/12/24.
+//
+
+import Foundation
