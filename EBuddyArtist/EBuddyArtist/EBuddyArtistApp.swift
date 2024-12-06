bbe4de625e7b143bcc5f40d8303d898e5ad986e7
macAji <ajienergystar@gmail.com>
2024-12-06 10:09:39 +0700

    Firebase Setup. Firestore, Storage and Auth.

---
+++ EBuddyArtist/EBuddyArtist/EBuddyArtistApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBuddyArtistApp.swift
+//  EBuddyArtist
+//
+//  Created by mac on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EBuddyArtistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
